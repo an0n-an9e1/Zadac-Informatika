e097eeb66eac8578925facd78b0febedd60549d0
Trunk <>
2025-11-26 09:40:48 +0100

    +

---
+++ src/kreiranje/A.java
@@ -0,0 +1,11 @@
+package kreiranje;
+
+public class A {
+	public void test() {
+		System.out.println("Hi");
+	}
+	public void zbir(int a, int b) {
+		int c = a + b;
+		System.out.println(c);
+	}
+}
---
+++ src/kreiranje/B.java
@@ -0,0 +1,9 @@
+package kreiranje;
+
+class B {
+	public static void main(String[] args) {
+		A obj = new A();
+		obj.test();
+		obj.zbir(3, 4);
+	}
+}
